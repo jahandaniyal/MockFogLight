1a1cbb24cdbb2888c2e5827c7110f3c530016411
planas <apostolisplanas@gmail.com>
2020-01-25 20:20:45 +0100

    Create dictionary from parsed json and if field doesnt exists continues

--- agent/server.py
+++ agent/server.py
@@ -2,10 +2,10 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 from io import BytesIO
 import json
 
+
 class WebServerHandler(BaseHTTPRequestHandler):
 
     def do_POST(self):
-
         self.send_response(200)
         content_length = int(self.headers['Content-Length'])
         content_type = self.headers['Content-Type']
@@ -22,17 +22,18 @@ class WebServerHandler(BaseHTTPRequestHandler):
 
         content_string = body.decode('utf-8')
         content_json_array = json.loads(content_string)
-        print(content_json_array)
-        # content_dict = {}
-        #
-        # content_dict['timestamp'] = content_json_array[0]['timestamp']
-        # for element in content_json_array['data'][0]:
-        #     content_dict['name'] = element['name']
-        #     content_dict['cpu'] = element['cpu']
-        #     content_dict['memory'] = element['memory']
-        #
-        # print(content_dict)
+        content_dict = {}
 
+        try:
+            content_dict['timestamp'] = content_json_array[0]['timestamp']
+            content_dict['name'] = content_json_array[0]['data']['name']
+            content_dict['cpu'] = content_json_array[0]['data']['cpu']
+            content_dict['memory'] = content_json_array[0]['data']['memory']
+            content_dict['empty'] = content_json_array[0]['data']['empy']
+        except KeyError:
+            pass
+
+        print(content_dict)
 
     def do_GET(self):
         self.send_response(200)
