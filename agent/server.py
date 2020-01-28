5cb9912b0755c890c0b7bae4e9e662e7119eb541
planas <apostolisplanas@gmail.com>
2020-01-28 19:23:58 +0100

    Removed message

--- agent/server.py
+++ agent/server.py
@@ -38,7 +38,6 @@ class WebServerHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(b'Hello, world!')
 
 
 def main():
